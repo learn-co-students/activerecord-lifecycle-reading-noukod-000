3b5f17f95a91d1fdd7d78b5723fdac370423076d
clifford <clifford@noukod.com>
2019-02-04 15:37:40 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,11 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
-  validate :is_title_case 
+  validate :is_title_case
 
+  # New Code!!
+  before_save :make_title_case
+  
   private
 
   def is_title_case
