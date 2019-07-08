3a86661fd4dae8f1d780b390aed3f0550854e84e
Herna Michel <labellehernamichel@yahoo.fr>
2019-07-08 01:51:16 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  # resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  resources :authors
+  resources :posts
 end
